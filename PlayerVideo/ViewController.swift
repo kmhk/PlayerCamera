37e5e27672567993d869fb8179979f9f29427448
user <user@users-Mac.local>
2019-03-20 06:11:53 +0300

    Initial Commit

---
+++ PlayerVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayerVideo
+//
+//  Created by user on 3/20/19.
+//  Copyright © 2019 KMHK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
